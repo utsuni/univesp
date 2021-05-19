0ac341081413eb1300026ea43a2111a5f4c027e7
Aline Utsuni <alineutsuni10@gmail.com>
2021-05-18 21:00:45 -0300

    2 finalização lista 1

--- Lista1/ex10.java
+++ Lista1/ex10.java
@@ -1,32 +1,32 @@
-class ex10 {
+		class ex10 {
 
-  public static void main(String[] argn) {
-    int jubiscleu = 987; // Resposta: 789
-    int reultado =  HEBE(jubiscleu);
-    // Criar função INV(numero): Dado um número inteiro passado para a função,
-    // retornar o valor e depois de retornar, exibir na tela
-	// System.out.printf("O inverso é: %d%d%d \n", terc_digito, segn_digito, prim_digito);
-	System.out.printf("O inverso é: %d \n", reultado);
-    // int d2 = 876;
-    // int d3 = 321;
-    // System.out.println("O inverso do numero " + d1 + +d2 + +d3 + " e " + d3 + +d2 + +d1);
-  }
+			public static void main(String[] argn) {
+		     int jubiscleu = 987; // Resposta: 789
+		     // int reultado =  HEBE(jubiscleu); //
+		     // Criar função INV(numero): Dado um número inteiro passado para a função,
+		     // retornar o valor e depois de retornar, exibir na tela
+		     // System.out.printf("O inverso é: %d%d%d \n", terc_digito, segn_digito, prim_digito);
+		     // System.out.printf("O inverso é: %d \n", reultado);
+		     System.out.printf("O inverso é: %d \n", HEBE(jubiscleu));
+		     // int d2 = 876;
+		     // int d3 = 321;
+		     // System.out.println("O inverso do numero " + d1 + +d2 + +d3 + " e " + d3 + +d2 + +d1);
+		 }
 
-  static int HEBE(int numero){
-  int prim_digito = 60000;
-	int segn_digito = 0;
-	int terc_digito = -11111;
-    
-	// Testar arredondamento e verificar o que é que
-	// vai acontecer
+	static int HEBE(int numero){
+		int prim_digito = 60000;
+		int segn_digito = 0;
+		int terc_digito = -11111;
 
-	// Math.floor: pega a divisão inteira pra baixo
-	terc_digito = numero % 10;
-	int div_inteira = (int) Math.floor(numero / 10);
-	segn_digito = div_inteira % 10;
-	prim_digito = (int) Math.floor(div_inteira / 10);
-	// fazer operaçao matematica orreta
-   	return (prim_digito*10) + (segn_digito*100) + (terc_digito*1000);
-  }
+		// Testar arredondamento e verificar o que é que
+		// vai acontecer
+
+		// Math.floor: pega a divisão inteira pra baixo
+		terc_digito = numero % 10;
+		int div_inteira = (int) Math.floor(numero / 10);
+		segn_digito = div_inteira % 10;
+		prim_digito = (int) Math.floor(div_inteira / 10);
+		// fazer operaçao matematica orreta
+		return (prim_digito*1) + (segn_digito*10) + (terc_digito*100);
+	}
 }
-     
--- Lista1/ex11.java
+++ Lista1/ex11.java
@@ -1,33 +1,31 @@
-class ex11{
-     
+  class ex11 {
+
      static int a,b,c;
-     
-     static void P(int x, int y) {
-      a = x+y+a; 
-     System.out.println(x+" "+y+" "+a);
-}                       
 
-    public static void main(String[] args) {
-    a = 5;                          
-    b = 8;                       
-    c = 3;                       
-      
-      // A = 18      //B = 8    //C = 3
-      // A = 54
-      // A = 492
+      static void P(int x, int y) {
+        a = x+y+a; 
+        System.out.println(x+" "+y+" "+a);
+      }                       
 
-    P(a,b); //(5,8) 
-     //a = x+y+a; (A = 5 + 8 + 5 = 18 )
-     //println(5+" "+8+" "+18)
-                     
-    P(7,a+b+c); // (7,18 + 8 + 3) = 29
-           //a = x+y+a; (a = 7 + 29 + 18 = 54 )
-         // println(7+" "+29+" "+54);  
+      public static void main(String[] args) {
+        a = 5;                          
+        b = 8;                       
+        c = 3;                       
 
-    P(a*b,a%b); //(54*8,54%8) 
-     //a = x+y+a; (a = 432 + 6 + 54 = 492 )
-     //println(432+" "+6+" "+492)
-    }
+        // A = 18      //B = 8    //C = 3
+        // A = 54
+        // A = 492
+
+        P(a,b); //(5,8) 
+        //a = x+y+a; (A = 5 + 8 + 5 = 18 )
+        //println(5+" "+8+" "+18)
+
+        P(7,a+b+c); // (7,18 + 8 + 3) = 29
+        //a = x+y+a; (a = 7 + 29 + 18 = 54 )
+        // println(7+" "+29+" "+54);  
+
+        P(a*b,a%b); //(54*8,54%8) 
+        //a = x+y+a; (a = 432 + 6 + 54 = 492 )
+        //println(432+" "+6+" "+492)
+  }
 }
-
-
